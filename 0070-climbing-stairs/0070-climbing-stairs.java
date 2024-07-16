279378ffbf7b7eac0dd2934f60be4c8031ec5af7
Keerthivasan <131132737+Keerthivasan-04@users.noreply.github.com>
2024-07-16 15:35:50 +0530

    Time: 0 ms (100%), Space: 40.4 MB (19.9%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -15,3 +15,14 @@ class Solution {
         return dp[n];
     }
 }
+
+// class Solution {
+//     public int climbStairs(int n) {
+        
+//         if (n == 0 || n==1) 
+//             return 1;
+        
+//         return climbStairs(n-1) + climbStairs(n-2);
+//     }
+// }
+    
