5baa0b793bd8ef275c0ed3c54b5d76ebd74a489c
Keerthivasan <131132737+Keerthivasan-04@users.noreply.github.com>
2024-07-19 10:21:23 +0530

    Time: 45 ms (30.04%), Space: 44.8 MB (36.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,35 +1,35 @@
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-
-//         int [] n = new int [2];
-//         for(int i =0;i<nums.length;i++){
-//             for(int j =i+1;j<nums.length;j++){
-//                 if(nums[i]+nums[j]==target){
-//                     n[0]=i;
-//                     n[1]=j;
-//                     break;
-//                 }
-                
-//             }
-//         }
-//      return n;
-        
-//     }
-// }
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer,Integer> h = new HashMap<> ();
 
-        for(int i=0;i<nums.length;i++){
-
-            if(h.containsKey(target - nums[i])){
-                return new int[]{h.get(target - nums[i]),i};
+        int [] n = new int [2];
+        for(int i =0;i<nums.length;i++){
+            for(int j =i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    n[0]=i;
+                    n[1]=j;
+                    break;
+                }
+                
             }
-           
-            h.put(nums[i],i);
-            
         }
-        return new int[]{-1 , -1};
+     return n;
+        
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap <Integer,Integer> h = new HashMap<> ();
+
+//         for(int i=0;i<nums.length;i++){
+
+//             if(h.containsKey(target - nums[i])){
+//                 return new int[]{h.get(target - nums[i]),i};
+//             }
+           
+//             h.put(nums[i],i);
+            
+//         }
+//         return new int[]{-1 , -1};
+//     }
+// }
