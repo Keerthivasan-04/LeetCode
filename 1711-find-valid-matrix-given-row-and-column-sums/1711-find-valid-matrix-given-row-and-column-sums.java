1bd79bd9fa2b7e6eabc807d88a3947642e13ad1d
Keerthivasan <131132737+Keerthivasan-04@users.noreply.github.com>
2024-07-20 10:51:49 +0530

    Time: 6 ms (76.89%), Space: 52.9 MB (41.43%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.java
@@ -1,52 +1,52 @@
-// class Solution {
-//     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
 
-//         int n = rowSum.length;
-//         int m = colSum.length;
+        int n = rowSum.length;
+        int m = colSum.length;
 
-//         int[][] ans = new int[n][m];
+        int[][] ans = new int[n][m];
 
-//         for(int i = 0; i < n; i++){
-//             for(int j = 0; j < m; j++){
-//                 int x = Math.min(rowSum[i],colSum[j]);
-//                 ans[i][j] = x;
-//                 rowSum[i] -= x;
-//                 colSum[j] -= x;
-//             }
-//         }
-//         return ans;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                int x = Math.min(rowSum[i],colSum[j]);
+                ans[i][j] = x;
+                rowSum[i] -= x;
+                colSum[j] -= x;
+            }
+        }
+        return ans;
         
-//     }
-// }
+    }
+}
 
 
 
 // Optimised Solution 1 ms 
-class Solution {
-    public int[][] restoreMatrix(int[] r, int[] c) {
+// class Solution {
+//     public int[][] restoreMatrix(int[] r, int[] c) {
 
-        int n = r.length;
-        int m = c.length;
+//         int n = r.length;
+//         int m = c.length;
 
-        int[][] ans = new int[n][m];
+//         int[][] ans = new int[n][m];
 
-        int i = n - 1;
-        int j = m - 1;
+//         int i = n - 1;
+//         int j = m - 1;
 
-        while(i >= 0 && j >= 0){
+//         while(i >= 0 && j >= 0){
 
-            if(r[i] <= c[j]){
-                ans[i][j] = r[i];
-                c[j] -= r[i];
-                i--;
-            }
+//             if(r[i] <= c[j]){
+//                 ans[i][j] = r[i];
+//                 c[j] -= r[i];
+//                 i--;
+//             }
 
-            else{
-                ans[i][j] = c[j];
-                r[i] -= c[j];
-                j--; 
-            }
-        }
-        return ans;
-    }
-}
+//             else{
+//                 ans[i][j] = c[j];
+//                 r[i] -= c[j];
+//                 j--; 
+//             }
+//         }
+//         return ans;
+//     }
+// }
