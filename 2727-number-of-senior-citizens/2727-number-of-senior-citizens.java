3d87c4e26559a58b9673fd0e02eff62e446f0c45
Keerthivasan <131132737+Keerthivasan-04@users.noreply.github.com>
2024-08-01 07:01:38 +0530

    Time: 1 ms (82.64%), Space: 42.8 MB (27.08%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -1,28 +1,28 @@
-// class Solution {
-//     public int countSeniors(String[] details) {
-//         int c = 0;
-//         for(String d : details){
-//             int a = Integer.parseInt(d.substring(11,13));
-//             if(a > 60){
-//                 c++;
-//             }
-//         }
-//         return c;
-//     }
-// }
-
-
 class Solution {
     public int countSeniors(String[] details) {
         int c = 0;
-        for(int i = 0; i < details.length; i++){
-            if(details[i].charAt(11) - '0' == 6 && details[i].charAt(12) - '0' >= 1){
-                c++;
-            }
-            else if(details[i].charAt(11)-'0' > 6){
+        for(String d : details){
+            int a = Integer.parseInt(d.substring(11,13));
+            if(a > 60){
                 c++;
             }
         }
         return c;
     }
 }
+
+
+// class Solution {
+//     public int countSeniors(String[] details) {
+//         int c = 0;
+//         for(int i = 0; i < details.length; i++){
+//             if(details[i].charAt(11) - '0' == 6 && details[i].charAt(12) - '0' >= 1){
+//                 c++;
+//             }
+//             else if(details[i].charAt(11)-'0' > 6){
+//                 c++;
+//             }
+//         }
+//         return c;
+//     }
+// }
