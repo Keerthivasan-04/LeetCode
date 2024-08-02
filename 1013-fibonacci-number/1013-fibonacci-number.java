d7bd5dc3241d967e9f4f7b0aa9716c62ddd3979f
Keerthivasan <131132737+Keerthivasan-04@users.noreply.github.com>
2024-08-02 12:12:27 +0530

    Time: 0 ms (100%), Space: 40 MB (86.05%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,17 +1,35 @@
+// memorization
+// class Solution {
+
+//     public int fib(int n) {
+//         int [] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return solve(n,dp);
+//     }
+//     public static int solve(int n , int[] dp){
+//         if(n<=1){
+//             return n;
+//         }
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }    
+//         return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+//     }
+// }
+
+// Tabulation
 class Solution {
 
     public int fib(int n) {
         int [] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
-    }
-    public static int solve(int n , int[] dp){
         if(n<=1){
             return n;
         }
-        if(dp[n] != -1){
-            return dp[n];
-        }    
-        return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i < n +1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
