94c34fbffd4e82cbf8aee503a84edcac87c48ad0
Keerthivasan <131132737+Keerthivasan-04@users.noreply.github.com>
2025-05-28 19:27:55 +0530

    Time: 0 ms (100%), Space: 40.1 MB (96.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,45 +1,14 @@
-// Memoization
 class Solution {
     public int climbStairs(int n) {
+        if(n == 1)  return n;
 
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
-    }
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
 
-    public static int solve(int n , int[] dp){
-        if(n <= 2) return n;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
-// Tabulation
-// class Solution {
-//     public int climbStairs(int n) {
-        
-//         if (n == 1) return 1;
-//         if (n == 2) return 2;
-        
-//         int[] dp = new int[n + 1];
-//         dp[1] = 1;
-//         dp[2] = 2;
-        
-//         for (int i = 3; i <= n; i++) {
-//             dp[i] = dp[i - 1] + dp[i - 2];
-//         }
-        
-//         return dp[n];
-//     }
-// }
-
-// Recursion
-// class Solution {
-//     public int climbStairs(int n) {
-        
-//         if (n == 0 || n==1) 
-//             return 1;
-        
-//         return climbStairs(n-1) + climbStairs(n-2);
-//     }
-// }
-    
