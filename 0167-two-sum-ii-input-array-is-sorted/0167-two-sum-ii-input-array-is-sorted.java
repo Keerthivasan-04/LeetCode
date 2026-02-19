4196472d5a5cf21e276ef3d1e2b094fab0fae2d4
Keerthivasan A <131132737+Keerthivasan-04@users.noreply.github.com>
2026-02-19 20:12:45 +0530

    Time: 405 ms (7.17%), Space: 48.7 MB (22.66%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,22 +1,34 @@
 class Solution {
     public int[] twoSum(int[] n, int target) {
-        int left = 0;
-        int right = n.length - 1;
+        // int[] res = new int[2];
 
-        while(left < right){
-            int sum = n[left] + n[right];
-            if(sum < target){
-                left++;
-            }
-            else if(sum > target){
-                right--;
-            }
-            else{
-                break;
+        for(int i = 0; i < n.length; i++){
+            for(int j = i + 1; j < n.length; j++){
+                if(n[i] + n[j] == target){
+                    return new int[]{i+1, j+1};
+                }
             }
         }
+        return new int[]{-1,-1};
+
+
+        // int left = 0;
+        // int right = n.length - 1;
+
+        // while(left < right){
+        //     int sum = n[left] + n[right];
+        //     if(sum < target){
+        //         left++;
+        //     }
+        //     else if(sum > target){
+        //         right--;
+        //     }
+        //     else{
+        //         break;
+        //     }
+        // }
         
-        return new int[]{left + 1, right + 1};
+        // return new int[]{left + 1, right + 1};
         
     }
 }
