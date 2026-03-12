c6bdbdbbe6daf4d5505b1d9b42523e84d1c46f21
Keerthivasan A <131132737+Keerthivasan-04@users.noreply.github.com>
2026-03-12 19:30:50 +0530

    Time: 1 ms (98.57%), Space: 47.6 MB (36.85%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -2,23 +2,41 @@ class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
 
-        int[] leftSum = new int[n];
-        leftSum[0] = nums[0];
-        for(int i = 1; i < n; i++){
-            leftSum[i] = nums[i] + leftSum[i - 1];
-        }
+        int total = 0;
+        for(int x : nums) total += x;
 
-        int[] rightSum = new int[n];
-        rightSum[n - 1] = nums[n - 1];
-        for(int i = n - 2; i >= 0; i--){
-            rightSum[i] = nums[i] + rightSum[i + 1];
-        }
-        
+        int left = 0;
         for(int i = 0; i < n; i++){
-            if(leftSum[i] == rightSum[i]){
+            if(left == total - left - nums[i]){
                 return i;
             }
+            left += nums[i];
         }
         return -1;
+
+
+
+
+
+
+
+        // int[] leftSum = new int[n];
+        // leftSum[0] = nums[0];
+        // for(int i = 1; i < n; i++){
+        //     leftSum[i] = nums[i] + leftSum[i - 1];
+        // }
+
+        // int[] rightSum = new int[n];
+        // rightSum[n - 1] = nums[n - 1];
+        // for(int i = n - 2; i >= 0; i--){
+        //     rightSum[i] = nums[i] + rightSum[i + 1];
+        // }
+        
+        // for(int i = 0; i < n; i++){
+        //     if(leftSum[i] == rightSum[i]){
+        //         return i;
+        //     }
+        // }
+        // return -1;
     }
 }
